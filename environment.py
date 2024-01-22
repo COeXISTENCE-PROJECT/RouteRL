973d442fbc56cd6ba226280b09f49c868cc14a27
Anastasia Psarou <97515093+AnastasiaPsarou@users.noreply.github.com>
2024-01-22 11:37:23 +0100

    Calculate reward function

--- environment.py
+++ environment.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import random
 from prettytable import PrettyTable
+import numpy as np
 
 from keychain import Keychain as kc
 from simulator import Simulator
@@ -32,12 +33,29 @@ class TrafficEnvironment:
 
     def step(self, joint_action):   # For now, returns random rewards
         agent_ids = joint_action[kc.AGENT_ID]
-        rewards = [-1 * random.uniform(20, 50) for i in range(len(agent_ids))]
+
+        ####
+        #### Feed agents actions to SUMO and get travel times
+        ####
+
+        ###### random - will change
+        sumo_df = pd.DataFrame({
+            'id': agent_ids,
+            'travel_times': np.random.uniform(low=1, high=100, size=len(agent_ids))
+        })
+
+        #### Calculate joint reward based on travel times returned by SUMO
+        joint_reward = self.calculate_rewards(sumo_df)
+
+        rewards = [joint_reward for i in range(len(agent_ids))]
         joint_reward = pd.DataFrame({kc.AGENT_ID : agent_ids, kc.REWARD : rewards})
+
         return joint_reward, None, True
 
-    def calculate_rewards(self):
-        pass
+    def calculate_rewards(self, sumo_df):
+        average_travel_time = sumo_df['travel_times'].mean()
+
+        return average_travel_time
 
     def print_agents(self, agents, print_every=1): # Should this be even here?
         table = PrettyTable()
