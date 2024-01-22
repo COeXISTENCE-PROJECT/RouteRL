a8838599866c6ed532d5108b54456f24f97bf781
Anastasia Psarou <97515093+AnastasiaPsarou@users.noreply.github.com>
2024-01-22 13:53:34 +0100

    SUMO changes

--- simulator.py
+++ simulator.py
@@ -4,6 +4,7 @@ import pandas as pd
 import traci
 import xml.etree.ElementTree as ET
 from Decision_model import logit
+from learning_model import gawron
 
 
 class Simulator:
@@ -20,6 +21,10 @@ class Simulator:
         #self.empty_route_2 = empty_route_2
         #self.beta = beta
         #self.nr_iterations = nr_iterations
+        self.sumo_type = 'sumo'
+        self.config = '../Network_and_config/csomor1.sumocfg'
+        self.simulation_length = 3600
+        self.beta = - 0.1
 
         # network x graph
         self.G = self.network('Network_and_config/csomor1.con.xml','Network_and_config/csomor1.edg.xml','Network_and_config/csomor1.rou.xml')# The network is build on a OSM map
@@ -29,9 +34,14 @@ class Simulator:
         self.route1 = self.iterate('279952229#0','-115602933#2', 'time',list(), -0.1,3)#I selected two source and target randomly, but here we can define anything else
         self.route2 = self.iterate('115604053','-441496282#1', 'time', list(),-0.1,3)
 
+        self.csv=pd.read_csv("agents_data.csv")
+        csv1= self.csv[self.csv.origin==0]
+        csv2= self.csv[self.csv.origin==1]
+
         # free flow travel time
         # maybe not for machines ???
-        self.in_time1, self.in_time2 = self.free_flow_time() 
+        self.cost1 = self.free_flow_time(self.route1, csv1) 
+        self.cost2 = self.free_flow_time(self.route2, csv2) 
 
         ## Init returns None
         
@@ -59,17 +69,24 @@ class Simulator:
             length.append(sum(rou))
 
         return length
+    
+    def free_flow_time(self, route, csv):
+        length=pd.DataFrame(self.G.edges(data=True))
 
-    def free_flow_time(self):
-        length = pd.DataFrame(self.G.edges(data=True))
-        time = length[2].astype('str').str.split(':',expand=True)[1]
-        length[2] = time.str.replace('}','',regex=True).astype('float')
+        time=length[2].astype('str').str.split(':',expand=True)[1]
+        length[2]=time.str.replace('}','',regex=True).astype('float')
 
-        #### Changed
-        in_time1 = self.free_flow_time_finder(self.route1, length[0], length[1], length[2])
-        in_time2 = self.free_flow_time_finder(self.route2, length[0], length[1], length[2])
+        in_time=self.free_flow_time_finder(route,length[0],length[1],length[2])
 
-        return in_time1, in_time2
+        cost=[]
+
+        for _ in range(len(csv)):
+            cost.append(in_time)
+
+        column_names = [f"cost{i+1}" for i in range(len(cost[0]))]
+        cost_df = pd.DataFrame(cost, columns=column_names)
+
+        return cost_df
     
 
 
@@ -96,9 +113,8 @@ class Simulator:
         # Extract <connection> elements with 'via' attribute
         rou = Bs_data_rou.find_all('edge', {'to': True})
 
-        empty=[]
-        for x in range(len(rou)):
-            empty.append(str(rou[x]))
+        empty = [str(rou[x]) for x in range(len(rou))]
+
 
         id=[]
         length=[]
@@ -128,10 +144,80 @@ class Simulator:
         final['time']=((final['length_x'].astype(float)/(final['speed_x'].astype(float)/3.6))/60)#np.exp
         final=final.drop(columns=['ID','length_y','speed_y','speed_x','length_x'])
         Graph = nx.from_pandas_edgelist(final, 'From', 'To', ['time'], create_using=nx.DiGraph())
-        
+    
         return Graph
+    
+    def run_simulation_iteration(self, time_route1,time_route2,route_1_rou_,route_2_rou_,route_1_veh_,route_2_veh_,cost1,cost2):#This is the siulation where we use the values from the initial simulation to improve the initial solutions of te cars
 
+        route_1,route_2= self.travel_time('tripinfo.xml',route_1_rou_,route_2_rou_,route_1_veh_,route_2_veh_)
 
+        time_route1= self.time_update(route_1,time_route1)
+        time_route2= self.time_update(route_2,time_route2)
+
+        # Start SUMO with TraCI
+        sumo_binary = self.sumo_type
+        sumo_cmd = [sumo_binary, "-c", self.config]
+        traci.start(sumo_cmd)
+        route_1_rou=[]
+        route_1_veh=[]
+        route_2_rou=[]
+        route_2_veh=[]
+        v=0
+
+        try:
+            # Set up demand (routes)
+            for i in range(len(self.route1)):
+                traci.route.add(f"route1_{i}", self.route1[i])
+            for i in range(len(self.route2)):
+                traci.route.add(f"route2_{i}", self.route2[i])
+
+            # Simulation loop
+            for _ in range(self.simulation_length):
+                traci.simulationStep()
+                #time_route1=time_route1
+                if _%60==0:
+                    time_route_1=list(time_route1.filter(like="cost").iloc[v])
+                    cost1_in=list(cost1.iloc[v])
+                    #route_1_1=pd.DataFrame(route_1.iloc[v]).T
+                    #time_route_1=time_upgrade(route_1_1,time_route1[v])
+                    cost1_1=self.gawron(0.2,time_route_1,cost1_in)
+                    #time_route1[v]=time_route_1
+                    cost1.iloc[v]=cost1_1
+                    j=logit(self.beta,cost1_1)
+                    #j=j.index(max(j))
+                    #time_route2=time_route2
+                    #route_2_1=pd.DataFrame(route_2.iloc[v]).T
+                    #time_route_2=time_upgrade(route_2_1,time_route2[v])
+                    #time_route2[v]=time_route_2
+                    time_route_2=list(time_route2.filter(like="cost").iloc[v])
+                    cost2_in=list(cost2.iloc[v])
+                    cost2_2=self.gawron(0.2,time_route_2,cost2_in)
+                    cost2.iloc[v]=cost2_2
+                    #time_route1[v]=time_route_1
+                    k=logit(self.beta,cost2_2)
+                    #k=k.index(max(k))
+                    vechicle_id1=time_route1.car[v]
+                    vechicle_id2=time_route2.car[v]
+                    #vechicle_id1=route_1.ID[v]
+                    #vechicle_id2=route_2.ID[v]
+                    traci.vehicle.add(vechicle_id1,f'route1_{j}')
+                    traci.vehicle.add(vechicle_id2,f'route2_{k}')
+                    traci.vehicle.setColor(vechicle_id2,(255,0,255))
+                    route_1_rou.append(j)
+                    route_1_veh.append(vechicle_id1)
+                    route_2_rou.append(k)
+                    route_2_veh.append(vechicle_id2)
+                    v+=1
+                    #Retrieve information using TraCI functions
+                    # For example, get vehicle positions, routes, travel times, etc.
+
+            # End of simulation
+        finally:
+            traci.close()
+
+        df1,df2=self.travel_time('tripinfo.xml',route_1_rou,route_2_rou,route_1_veh,route_2_veh)
+
+        return route_1_rou,route_1_veh,route_2_rou,route_2_veh,time_route1,time_route2,cost1,cost2,df1,df2
 
     
 
@@ -272,6 +358,31 @@ class Simulator:
         return paths
     
 
+    def travel_time(self, file_path,route_1_rou,route_2_rou,csv1,csv2,cost1,cost2):
+        id=pd.DataFrame(pd.read_xml(file_path).id).rename(columns={'id':'car_id'})
+
+        dur=pd.read_xml(file_path).rename(columns={'duration':'cost'}).cost/60
+
+        time=pd.merge(id,dur,right_index=True,left_index=True)
+        route_1=pd.merge(csv1,time,left_on='id',right_on='car_id',how='left')
+
+        route_1=route_1.drop(columns='car_id')
+        route_2=pd.merge(csv2,time,left_on='id',right_on='car_id',how='left')
+
+        route_2=route_2.drop(columns='car_id')
+
+        rou1=pd.DataFrame(route_1_rou).rename(columns={0:'route_id'})
+        rou2=pd.DataFrame(route_2_rou).rename(columns={0:'route_id'})
+
+        route_1=pd.merge(route_1,rou1,right_index=True,left_index=True)
+        route_2=pd.merge(route_2,rou2,right_index=True,left_index=True)
+
+        route_1=self.replace(route_1,cost1)
+        route_2=self.replace(route_2,cost2)
+
+        return route_1,route_2
+    
+
 
     def stringer(self, x):
         x = str(x).replace('"','')
@@ -299,3 +410,12 @@ class Simulator:
         to_db=pd.DataFrame(to_)
 
         return from_db, to_db
+    
+    def replace(times, zeros):
+
+        for x in range(len(times)):
+            for y in range(len(zeros)):
+
+                if pd.isna(times.cost[y]) and times.index[x]==zeros.index[y]:
+                        times.at[x,'cost']=zeros.iloc[:,times.route_id[x]][y]
+        return times
