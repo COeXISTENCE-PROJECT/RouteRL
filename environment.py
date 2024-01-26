b51a37f0c17c53e0e003b52f4d8d6c6753ed642c
Anastasia Psarou <97515093+AnastasiaPsarou@users.noreply.github.com>
2024-01-26 12:03:16 +0100

    changes

---
+++ environment.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import gymnasium as gym
+from gymnasium.spaces import Box
+from gymnasium.spaces import Discrete
+import random
+import traci
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+
+from keychain import Keychain as kc
+from simulator import Simulator
+from agent import Agent
+
+class TrafficEnvironment(gym.Env): ##inherit from gym
+
+    """
+    To be implemented
+    """
+
+    def __init__(self, simulation_parameters): # get params for simulator
+        # Initialize network
+        # Create demand
+        # Create paths
+        # Calculate free flows
+        # done
+        self.simulator = Simulator(simulation_parameters)  # pass params for simulator, and only the number of agents
+        self.reward_table = []
+        self.observation_space = Box(low=0, high=1, shape=(3,), dtype=float)
+
+        self.action_space = Discrete(3)
+        self.agents = []
+
+        print("[SUCCESS] Environment initiated!")
+
+
+
+    def calculate_free_flow_times(self):
+        free_flow_cost = self.simulator.calculate_free_flow_times()
+        print('[INFO] Free-flow times: ', free_flow_cost)
+        return free_flow_cost
+    
+    def create_agents(self, agents):
+        self.agents = agents        
+        
+
+    def reset(self):
+        #remove=traci.vehicle.getIDList()
+        #for i in range(len(remove)):
+        #    traci.vehicle.remove(remove[i],3)
+        
+        return [0, 0, 0]
+
+
+
+    def step(self, joint_action):
+        agent_ids = joint_action[kc.AGENT_ID]
+
+        ####
+        #### Feed agents actions to SUMO and get travel times
+        ####
+        simulation_length = 3600 ### fix it
+
+        sumo_df = self.simulator.run_simulation_iteration(simulation_length, joint_action)
+
+        #### Calculate joint reward based on travel times returned by SUMO
+        joint_reward = self.calculate_rewards(sumo_df)
+
+        rewards = [joint_reward for i in range(len(agent_ids))]
+        joint_reward = pd.DataFrame({kc.AGENT_ID : agent_ids, kc.REWARD : rewards})
+
+        return joint_reward, None, True
+
+
+    def calculate_rewards(self, sumo_df):
+        ### sychronize names
+        average_travel_time = -1 * sumo_df['cost'].mean()
+        self.reward_table.append(average_travel_time)
+
+        return average_travel_time
+    
+    def plot_rewards(self):
+
+        # Plotting
+        #print(self.reward_table)
+        plt.plot(self.reward_table)
+        plt.xlabel('Episode')
+        plt.ylabel('Reward')
+        plt.title('Reward Table Over Episodes')
+        plt.show()
+
+    def encode(state, ts_id):
+        """Encode the state of the traffic signal into a hashable object."""
+
+        ## state can be the number of vehicles in each path
+
+        # tuples are hashable and can be used as key in python dictionary
+        return state
--- human_learning/__init__.py
+++ human_learning/__init__.py
@@ -1,3 +1,2 @@
 from .decision_models import model
 from .decision_models import logit
-from .learning_models import gawron
