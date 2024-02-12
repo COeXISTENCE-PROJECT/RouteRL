fixing the freeflow calculation

services/simulator.py
@@ -191,7 +191,7 @@ class Simulator:
         final=pd.merge(id_name,from_to,right_on='From',left_on='ID')
         final=final.drop(columns=['ID'])
         final=pd.merge(id_name,final,right_on='To',left_on='ID')
-        final['time']=((final['length_x'].astype(float)/(final['speed_x'].astype(float)/3.6))/60)#np.exp
+        final['time']=((final['length_x'].astype(float)/(final['speed_x'].astype(float)))/60)
         final=final.drop(columns=['ID','length_y','speed_y','speed_x','length_x'])
         traffic_graph = nx.from_pandas_edgelist(final, 'From', 'To', ['time'], create_using=nx.DiGraph())
     
@@ -293,65 +293,3 @@ class Simulator:
     def sumonize_action(self, origin, destination, action):
         return f'{origin}_{destination}_{action}'
     
-
-
-    # DO WE EVEN USE THIS??
-    """
-    def create_network_from_xml(self, connection_file, edge_file, route_file):
-        # Connection file
-        from_db, to_db = self.read_xml_file(connection_file, 'connection', 'from', 'to')
-        from_to = pd.merge(from_db,to_db,left_index=True,right_index=True)
-        from_to = from_to.rename(columns={'0_x':'From','0_y':'To'})
-        
-        # Edge file
-        id_db, from_db = self.read_xml_file(edge_file, 'edge', 'id', 'from')
-
-        id_name = pd.merge(from_db,id_db,right_index=True,left_index=True)
-
-        id_name['0_x']=[remove_double_quotes(x) for x in id_name['0_x']]
-        id_name['0_y']=[remove_double_quotes(x) for x in id_name['0_y']]
-        id_name=id_name.rename(columns={'0_x':'Name','0_y':'ID'})
-        
-        # Route file
-        with open(route_file, 'r') as f:
-            data_rou = f.read()
-        Bs_data_rou = BeautifulSoup(data_rou, "xml")
-
-        # Extract <connection> elements with 'via' attribute
-        rou = Bs_data_rou.find_all('edge', {'to': True})
-
-        empty=[]
-        for x in range(len(rou)):
-            empty.append(str(rou[x]))
-
-        id=[]
-        length=[]
-        speed=[]
-        for x in range(len(empty)):
-            root = ET.fromstring(empty[x])
-            id.append(root.attrib.get('id'))
-            length.append(root.find('.//lane').attrib.get('length'))
-            speed.append(root.find('.//lane').attrib.get('speed'))
-        
-        id_db=pd.DataFrame(id)
-        len_db=pd.DataFrame(length)
-        speed_db=pd.DataFrame(speed)
-
-        speed_name=pd.merge(speed_db,id_db,right_index=True,left_index=True)
-        speed_name=speed_name.rename(columns={'0_x':'speed','0_y':'ID'})
-
-        len_name=pd.merge(len_db,id_db,right_index=True,left_index=True)
-        len_name=len_name.rename(columns={'0_x':'length','0_y':'ID'})
-
-        id_name=pd.merge(len_name,id_name,right_on='ID',left_on='ID')
-        id_name=pd.merge(speed_name,id_name,right_on='ID',left_on='ID')
-
-        final=pd.merge(id_name,from_to,right_on='From',left_on='ID')
-        final=final.drop(columns=['ID'])
-        final=pd.merge(id_name,final,right_on='To',left_on='ID')
-        final['time']=((final['length_x'].astype(float)/(final['speed_x'].astype(float)/3.6))/60)#np.exp
-        final=final.drop(columns=['ID','length_y','speed_y','speed_x','length_x'])
-        Graph = nx.from_pandas_edgelist(final, 'From', 'To', ['time'], create_using=nx.DiGraph())
-        
-        return Graph
-    """