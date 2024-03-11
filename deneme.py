5d57047d56a1453daba2e2f1935ed7e975102acb
aonurakman <ahmetonurakman@gmail.com>
2024-03-11 10:23:55 +0100

    oops

--- deneme.py
+++
@@ -1,7 +0,0 @@
-import torch 
-
-# check if CUDA is available
-train_on_gpu = torch.cuda.is_available()
-print(train_on_gpu)
-
-print([torch.cuda.device(i) for i in range(torch.cuda.device_count())])
