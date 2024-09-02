85f441db3c6a60b449afda137964c4c30a9785e0
Anastasia Psarou <anastasiapsarou123@gmail.com>
2024-09-02 14:50:02 +0200

    server changes

--- experiments/trials_on_two_routes/human_mutation_ppo.py
+++ experiments/trials_on_two_routes/human_mutation_ppo.py
@@ -39,7 +39,7 @@ print("device is: ", device)
 vmas_device = device  # The device where the simulator is run
 
 # Sampling
-frames_per_batch = 20  # Number of team frames collected per training iteration
+frames_per_batch = 200  # Number of team frames collected per training iteration
 n_iters = 100  # Number of sampling and training iterations - the episodes the plotter plots
 total_frames = frames_per_batch * n_iters
 
