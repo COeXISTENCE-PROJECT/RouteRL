cb95d933f127ff91e11ea891b9fb03f761dec9ec
Anastasia Psarou <anastasiapsarou123@gmail.com>
2024-09-04 09:02:43 +0200

    server exper

--- server_scripts/cmd_container.sh
+++ server_scripts/cmd_container.sh
@@ -2,7 +2,6 @@
 
 cd /app/
 echo "--- Installing dependencies ---"
-pip3 install --upgrade pip
 pip3 install --no-cache-dir -r requirements.txt
 echo "--- Running main.py ---"
-python experiments/trials_on_two_routes/human_mutation_ppo.py
+python experiments/trials_on_two_routes/mutation_dqn.py
