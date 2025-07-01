ab79f9575686504822cd74c344e47b0160f4c9f8
Anastasia Psarou <97515093+AnastasiaPsarou@users.noreply.github.com>
2025-07-01 13:34:39 +0200

    cmd-container-filename

--- server_scripts/cmd_container.sh
+++ server_scripts/cmd_container.sh
@@ -4,4 +4,4 @@ cd /app/
 echo "--- Installing dependencies ---"
 pip3 install --no-cache-dir -r requirements.txt
 echo "--- Running main.py ---"
-python -u main.py
+python -u tutorials/two_route_net/mappo_marginal.py
